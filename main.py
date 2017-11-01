40a3684f17e137bd32bc846b62fbbfd93a546be9
Jacob Pashman <jacobpashman@gmail.com>
2017-11-01 11:59:13 -0700

    tried to fix repeat

--- main.py
+++ main.py
@@ -30,7 +30,6 @@ class App(tk.Tk):
         self.filename = tkFileDialog.askopenfilename(title="Open file")
 
     def randomize(self):
-        alreadyChosen = []
         global repeat
         df = pd.read_excel(self.filename)
         nameList =  df.as_matrix()
@@ -39,17 +38,28 @@ class App(tk.Tk):
             if i>0 and i<len(nameList):
                 print nameList[i][0]
                 firstNames.append(nameList[i][0])
+        try:
+             alreadyChosen 
+             print 'does a exist in the current namespace'
+        except NameError:
+            alreadyChosen = firstNames 
+            print 'does exist'
+
         print 'first name list', firstNames
         number = randint(0,len(nameList)-2)
-        alreadyChosen.append(number)
+        while repeat.get() == 1 and (firstNames[number] not in alreadyChosen):
+            number = randint(0,len(nameList)-2)
+            print 'repeated'
         print "number is: ", number
-        check = True
-        for j in range(len(alreadyChosen)):
-            if j == number:
-                check = False
-                self.randomize()
-                break
         print 'THE NAME:', firstNames[number]
+        # alreadyChosen.remove(firstNames[number])
+        try:
+            del alreadyChosen[firstNames.index(firstNames[number])]
+            print 'index', firstNames.index(firstNames[number])
+        except:
+            print 'already deleted'
+        
+        print 'already chosen:', alreadyChosen
         tk.Label(self, text=firstNames[number]).pack()
         
 
